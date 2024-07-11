9d19a57ed63601a7268522f25b0ea5a9e0a7c742
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-07-11 14:01:45 +0100

    refactor LLM names into global constants

--- src/ask/main.py
+++ src/ask/main.py
@@ -4,11 +4,13 @@ import chromadb.utils.embedding_functions as embedding_functions
 import langdetect
 
 OLLAMA_URL = 'http://localhost:11434/api'
+LLM_MODEL = 'llama3:8b'
+EMBED_MODEL = 'nomic-embed-text'
 
 client = chromadb.PersistentClient()
 embed = embedding_functions.OllamaEmbeddingFunction(
     url=f"{OLLAMA_URL}/embeddings",
-    model_name="nomic-embed-text"
+    model_name=EMBED_MODEL
 )
 collection = client.get_or_create_collection('pdf-data', embedding_function=embed)
 
@@ -18,7 +20,7 @@ while True:
         question = input('Enter your question: ')
         
         # make the question english
-        response = ollama.chat(model='llama3:8b', messages=[
+        response = ollama.chat(model=LLM_MODEL, messages=[
             {
                 'role': 'user',
                 'content': f'translate this question using clear and concise english:\n\n\n{question}',
@@ -27,7 +29,7 @@ while True:
         transformed_question = response['message']['content']
 
         # perform the dummy search
-        response = ollama.chat(model='llama3:8b', messages=[
+        response = ollama.chat(model=LLM_MODEL, messages=[
             {
                 'role': 'user',
                 'content': f'create a short answer for this question:\n\n\n{transformed_question}',
@@ -38,7 +40,7 @@ while True:
         document = collection.query(query_texts=query)['documents'][0][0]
 
         # answer the user with the result
-        response = ollama.chat(model='llama3:8b', messages=[
+        response = ollama.chat(model=LLM_MODEL, messages=[
             {
                 'role': 'user',
                 'content': f'Using this context:\n\n\n{document}\n\n\nAnswer this question in clear concise english. Do not say anything other then the response:\n\n\n{question}',
@@ -50,7 +52,7 @@ while True:
         # translate to correct language
         language = langdetect.detect(question)
         language = "ukranian" if language == "uk" else "english"
-        response = ollama.chat(model='llama3:8b', messages=[
+        response = ollama.chat(model=LLM_MODEL, messages=[
             {
                 'role': 'user',
                 'content': f'Translate this text to {language}. Do not include extra notes or content:\n\n\n{final_response}',
--- src/ingest/main.py
+++ src/ingest/main.py
@@ -9,11 +9,13 @@ import chromadb.utils.embedding_functions as embedding_functions
 file = sys.argv[1]
 
 OLLAMA_URL = 'http://localhost:11434/api'
+LLM_MODEL = 'llama3:8b'
+EMBED_MODEL = 'nomic-embed-text'
 
 client = chromadb.PersistentClient()
 embed = embedding_functions.OllamaEmbeddingFunction(
     url=f"{OLLAMA_URL}/embeddings",
-    model_name="nomic-embed-text"
+    model_name=EMBED_MODEL
 )
 collection = client.get_or_create_collection('pdf-data', embedding_function=embed)
 
@@ -24,7 +26,7 @@ with pdfplumber.open(file) as pdf:
         text = page.extract_text()
 
         # translate and format the document
-        response = ollama.chat(model='llama3:8b', messages=[
+        response = ollama.chat(model=LLM_MODEL, messages=[
             {
                 'role': 'user',
                 'content': f'Translate this document without including any other content. Ignore page numbers, navigation information, organisers and other similar information:\n\n\n{text}',
@@ -34,7 +36,7 @@ with pdfplumber.open(file) as pdf:
         text = text.split('\n', 1)[-1].strip() # remove the first line "here is your translated document:" message
 
         # check if the text is actually useful for RAG
-        response = ollama.chat(model='llama3:8b', messages=[
+        response = ollama.chat(model=LLM_MODEL, messages=[
             {
                 'role': 'user',
                 'content': f'Do you think this document has any useful raw knowledge? reject anything that has slideshow information only (yes/no):\n\n\n{text}',
