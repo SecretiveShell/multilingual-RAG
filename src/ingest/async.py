f1ab3bdd45b6da048a7eacc22eead14782d548ed
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-08-04 19:03:17 +0100

    add page number to logging

--- src/ingest/async.py
+++ src/ingest/async.py
@@ -22,7 +22,9 @@ collection = client.get_or_create_collection('pdf-data', embedding_function=embe
 
 ollama = AsyncClient(host=OLLAMA_URL)
 
-async def parse_page(text) :
+async def parse_page(text, page_num, num_pages) :
+    pageid = f"{page_num}/{num_pages}"
+    print(f"[[Processing {pageid}]]")
 
     # translate and format the document
     response = await ollama.chat(model=LLM_MODEL, messages=[
@@ -34,6 +36,8 @@ async def parse_page(text) :
     text = response['message']['content']
     text = text.split('\n', 1)[-1].strip() # remove the first line "here is your translated document:" message
 
+    print(f"[[Translated {pageid}]]")
+
     # check if the text is actually useful for RAG
     response = await ollama.chat(model=LLM_MODEL, messages=[
         {
@@ -43,19 +47,22 @@ async def parse_page(text) :
     ])
     useful = response['message']['content'].split()[0].lower()
     if not "yes" in useful: 
-        print("[[Skipping]]")
+        print(f"[[Skipping {pageid}]]")
         return
+    
+    print(f"[[Adding {pageid}]]")
 
     hash = hashlib.sha1(text.encode('utf-32')).hexdigest()
     collection.add(ids=hash, documents=text)
 
-    print("[[Added]]")
+    print(f"[[Added {pageid}]]")
 
 async def main() :
     jobs = []
     with pdfplumber.open(file) as pdf:
-        for page in pdf.pages :
-            jobs.append(parse_page(page.extract_text()))
+        num_pages = len(pdf.pages)
+        for page_num, page in enumerate(pdf.pages) :
+            jobs.append(parse_page(page.extract_text(), page_num+1, num_pages))
 
     await asyncio.gather(*jobs)
     
